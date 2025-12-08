565e7cc9bc9c7bf73496543a67facf87b0ef1132
Forven47 <86151722+Forven47@users.noreply.github.com>
2025-12-08 14:20:10 +0800

    Modify patch script to update UnityPlayerActivity

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,4 +1,2 @@
-grep -q Perseus com.bilibili.AzurLane/smali_classes3/com/unity3d/player/UnityPlayerActivity.smali && exit
-oncreate=$(grep -n -m 1 'onCreate' com.bilibili.AzurLane/smali_classes3/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
-sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" com.bilibili.AzurLane/smali_classes3/com/unity3d/player/UnityPlayerActivity.smali
-sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Perseus\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" com.bilibili.AzurLane/smali_classes3/com/unity3d/player/UnityPlayerActivity.smali
+sed -i "/.method public constructor <init>()V/,/.end method/ s#    .locals 0#    .locals 0\n    invoke-static {}, Lcom/android/support/Main;->Start()V#" com.bilibili.AzurLane/smali_classes3/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "s#    </application>#        <service android:name=\"com.android.support.Launcher\" android:enabled=\"true\" android:exported=\"false\" android:stopWithTask=\"true\"/>\n    </application>\n    <uses-permission android:name=\"android.permission.SYSTEM_ALERT_WINDOW\"/>#" com.bilibili.AzurLane/AndroidManifest.xml
